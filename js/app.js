9a1ffc430c775e210b37691a35232df6d6b0444b
NicolasBiondini <nicolasbiondini29@gmail.com>
2020-09-28 15:40:44 -0300

    creacion de nuevas funciones y addEventListener al enviar datos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
-function Seguro() {
-
+function Seguro(marca, anio, seguro) {
+    this.marca = marca;
+    this.anio = anio;
+    this.seguro = seguro;
 }
 
+// Relacionado con la interfaz grafica que se va a mostrar
+function Interfaz() {};
+
+// Event Listener y recopilacion de datos
+
+const formulario = document.querySelector("#cotizar-seguro");
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    // leer la marca seleccionada
+    const marca = document.getElementById("marca");
+    const marcaSeleccionada = marca.options[marca.selectedIndex].value;
+    
+    // leer el año seleccionado
+    const anio = document.getElementById("anio");
+    const anioSeleccionado = anio.options[anio.selectedIndex].value;
+
+    // leer el tipo de seguro
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+    const newCar = new Seguro(marcaSeleccionada, anioSeleccionado, tipo);
+
+    console.log(newCar);
+})
 
 // Crear fechas del año
 const max = new Date().getFullYear();
